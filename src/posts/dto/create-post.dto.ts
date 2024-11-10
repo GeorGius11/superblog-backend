cc1af5968cbeeee5921ce70b5bb85d68c582d514
George Rybko <george.rybko@gmail.com>
2024-11-10 18:22:32 +0200

    create post dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+}
