e768afeb842189c4e015c4500bb96b6ae3378c42
George Rybko <george.rybko@gmail.com>
2024-11-10 18:07:25 +0200

    mongoose init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/superblog'),
+  ],
 })
 export class AppModule {}
