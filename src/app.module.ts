6767f64e4cd8ab21948913259216e6c0fa9289b0
George Rybko <george.rybko@gmail.com>
2024-11-10 19:16:15 +0200

    use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     PostsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/superblog'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class AppModule {}
