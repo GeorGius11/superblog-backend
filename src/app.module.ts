87a35bdeeb4bfa39d119ffe08f135694a4a7d740
George Rybko <george.rybko@gmail.com>
2024-11-11 02:02:12 +0200

    serve static images

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'images'),
+      serveRoot: '/images',
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     PostsModule,
     MongooseModule.forRootAsync({
